c0bc52f3924aa41eb1904c614eaebea368e6d048
Raj Vengala <rvengala@wrench.com>
2021-06-03 14:26:20 +0530

    added support for an application with many objects

--- mustang_core/lib/src/state/wrench_store.dart
+++ mustang_core/lib/src/state/wrench_store.dart
@@ -8,13 +8,36 @@ class WrenchStore {
   /// Looks up instance of type [T], if exists. Returns null if instance of
   /// type [T] is not found.
   static T get<T>() {
-    return _store.firstWhere((element) => element is T, orElse: () => null);
+    T t;
+    if (_largeStore) {
+      int typeHash = _getTypeHash(T.toString());
+      t = _hashStore[typeHash];
+    } else {
+      t = _store.firstWhere(
+        (element) => element is T,
+        orElse: () => null,
+      );
+    }
+    return t;
   }
 
   /// Saves instance [t] after removing, if exists, an instance of [T]
   static void update<T>(T t) {
-    _store.removeWhere((element) => element is T);
-    _store.add(t);
+    if (_largeStore) {
+      int typeHash = _getTypeHash(T.toString());
+      _hashStore.update(
+        typeHash,
+        (_) => t,
+        ifAbsent: () => t,
+      );
+    } else {
+      int objectIndex = _store.indexWhere((element) => element is T);
+      if (objectIndex != -1) {
+        _store.insert(objectIndex, t);
+      } else {
+        _store.add(t);
+      }
+    }
   }
 
   /// Saves instances [t] and [s] after removing, if exists,
@@ -44,13 +67,54 @@ class WrenchStore {
 
   /// Removes instance of type [T], if exists
   static void delete<T>() {
-    _store.removeWhere((element) => element is T);
+    if (_largeStore) {
+      int typeHash = _getTypeHash(T.toString());
+      _hashStore.remove(typeHash);
+    } else {
+      _store.removeWhere((element) => element is T);
+    }
   }
 
   // removes all objects from the store
   static void nuke() {
-    _store.clear();
+    if (_largeStore) {
+      _hashStore.clear();
+    } else {
+      _store.clear();
+    }
   }
 
-  static final HashSet<Object> _store = HashSet();
+  static void largeStore(bool status) {
+    nuke();
+    _largeStore = status;
+  }
+
+  static int _getTypeHash(String typeStr) {
+    return _jf(_jc(0, typeStr.hashCode));
+  }
+
+  // Source: built_value library
+  static int _jc(int hash, int value) {
+    // Jenkins hash "combine".
+    hash = 0x1fffffff & (hash + value);
+    hash = 0x1fffffff & (hash + ((0x0007ffff & hash) << 10));
+    return hash ^ (hash >> 6);
+  }
+
+  // Source: built_value library
+  static int _jf(int hash) {
+    // Jenkins hash "finish".
+    hash = 0x1fffffff & (hash + ((0x03ffffff & hash) << 3));
+    hash = hash ^ (hash >> 11);
+    return 0x1fffffff & (hash + ((0x00003fff & hash) << 15));
+  }
+
+  // Flag to switch between List and HashMap
+  static bool _largeStore = true;
+
+  // List is used to store objects when _largeStore flag is disabled
+  static final List<Object> _store = [];
+
+  // HashMap is used to store objects when _largeStore flag is enabled
+  static final HashMap<int, Object> _hashStore = HashMap();
 }
