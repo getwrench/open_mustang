386cde9badd2036bc6f502aee93a4f3a06cfa41a
A <amit.jha6700@gmail.com>
2022-01-20 03:59:49 +0530

    aspect_visitor comment update

--- mustang_codegen/lib/src/aspect_visitor.dart
+++ mustang_codegen/lib/src/aspect_visitor.dart
@@ -2,7 +2,9 @@ import 'package:analyzer/dart/element/element.dart';
 import 'package:analyzer/dart/element/visitor.dart';
 import 'package:mustang_core/mustang_core.dart';
 
-/// Visits an generated aspect file and finds available hooks
+/// Visits a generated aspect file and finds available hooks
+/// used in [HookOverrideGenerator] to find which hooks are available
+/// for a generated aspect
 class AspectVisitor extends SimpleElementVisitor {
   const AspectVisitor(this.availableMethods);
 
