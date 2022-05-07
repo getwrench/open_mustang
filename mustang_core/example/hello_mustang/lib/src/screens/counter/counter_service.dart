267a849510e8eeb65e502313deeb8b801813443c
Raj Vengala <rvengala@wrench.com>
2022-05-07 16:10:37 +0530

    updated example

--- mustang_core/example/hello_mustang/lib/src/screens/counter/counter_service.dart
+++ mustang_core/example/hello_mustang/lib/src/screens/counter/counter_service.dart
@@ -2,13 +2,8 @@ import 'package:hello_mustang/src/models/counter.model.dart';
 import 'package:hello_mustang/src/screens/counter/counter_service.service.dart';
 import 'package:mustang_core/mustang_core.dart';
 
-// import 'counter_service.service.dart';
-
 @screenService
 abstract class $CounterService {
-  void clearCacheAndReload({bool reload = true}) {
-    clearMemoizedScreen(reload: reload);
-  }
 
   void increment() {
     Counter counter = MustangStore.get<Counter>() ?? Counter();
