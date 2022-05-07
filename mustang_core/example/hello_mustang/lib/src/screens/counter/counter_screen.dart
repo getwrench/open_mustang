8b34c7bd76f3ded642d1259c53113b35d9f81796
Raj Vengala <rvengala@wrench.com>
2022-05-07 16:09:06 +0530

    updated example

--- mustang_core/example/hello_mustang/lib/src/screens/counter/counter_screen.dart
+++ mustang_core/example/hello_mustang/lib/src/screens/counter/counter_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:hello_mustang/src/screens/counter/greeting.dart';
+import 'package:hello_mustang/src/screens/counter/widgets/greeting.dart';
 import 'package:mustang_core/mustang_widgets.dart';
 
 import 'counter_service.service.dart';
